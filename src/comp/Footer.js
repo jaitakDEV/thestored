f70f870e58ff43ca55c25a3bad3c37667ddf68c8
jaitak7 <74637946+jaitak7@users.noreply.github.com>
2023-12-30 14:48:59 +0530

    Update Footer.js

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
 Welcome to TheStored—your ultimate destination where style meets convenience. At TheStored, we're committed to transforming your online shopping experience. Our curated collections blend quality, innovation, and trendsetting products. Established in [Year], TheStored began with a passion for delivering exceptional items to customers worldwide. As we evolve, we invite you to be part of our story. Join us on social media, explore our curated selection, and experience the joy of shopping redefined. Thank you for choosing TheStored—your trusted destination for a seamless and stylish online shopping adventure.
 
         </div>
-        <div className="foot">© theStored {year.getFullYear()}</div>
+        <div className="foot">Copyright All rights reserved | by  Jai Tak {year.getFullYear()}</div>
       </div>
     </>
   );
